250a0b39c663bba7af0ce0a7c17a18c239ecb0e1
java01 <sergo231993@gmail.com>
2020-07-21 20:57:41 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+	String text = new String(Files.readAllBytes(Paths.get("C:\\Users\\Java01\\Desktop\\Harry.txt")));
+	String first200 = text.substring(0,200);
+
+        /*System.out.println(first200);*/
+
+        String textTrashEree = text.replaceAll("[^A-Za-z`\\- ]","");
+       /* first200 = textTrashEree.substring(0,2000);*/
+       /* System.out.println(first200);*/
+
+        String [] wordsArrey = textTrashEree.split(" ");
+        System.out.println(wordsArrey.length);
+
+        for (int i = 0; i <wordsArrey.length ; i++) {
+
+                //всі слова, довжина яких >=3
+            if ( (wordsArrey[i].length()>=3)  && (wordsArrey[i].charAt(0)=='a') ) {
+                                                //перша буква ( charAt(0) ) слова "а"
+                                                // символ charAt береться у одинокі скобки ('')
+
+
+                System.out.print(i + ". " );        //виводить номер і точку
+                System.out.println(wordsArrey[i]);      //виводить слово на якому номері
+            }
+
+        }
+
+
+    }
+}
